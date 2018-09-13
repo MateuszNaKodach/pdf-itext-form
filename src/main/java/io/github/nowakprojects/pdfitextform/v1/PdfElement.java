1df1903ab9fecf345ee62f1ba5c817bd126f07d3
mnowak <mateusz.nowak@intenso.pl>
2018-09-13 16:09:40 +0200

    Delete old version.

--- src/main/java/io/github/nowakprojects/pdfitextform/v1/PdfElement.java
+++
@@ -1,9 +0,0 @@
-package io.github.nowakprojects.pdfitextform.v1;
-
-interface PdfElement {
-
-    default boolean isAbsoluteText() {
-        return this instanceof AbsoluteTextPdfElement;
-    }
-
-}
--- src/main/java/io/github/nowakprojects/pdfitextform/v1/PositionType.java
+++
@@ -1,6 +0,0 @@
-package io.github.nowakprojects.pdfitextform.v1;
-
-enum PositionType {
-    FROM_TOP_LEFT,
-    FROM_BOTTOM_LEFT;
-}
